c2b6be40df0edf78c4399ab66560c8ecf1902870
joey-largent <177733313+joey-largent@users.noreply.github.com>
2024-11-24 15:32:50 +0100

    add score variables

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 //COMPUTER CHOICE
 
-//first we generate a random number between 1-3
+//generate a random number between 1-3
 function getRandomNum(min, max) {
     min = Math.ceil(1);
     max = Math.floor(3);
     return number = Math.floor(Math.random() * (3 - 1 + 1)) + 1;
 };
 
-//then we need to make a conditional statement to link the numbers with strings for R, P, and S
-//we also need to link the two functions using "number"
+//link the numbers with strings for R, P, and S
+//link the two functions using "number"
 function getComputerChoice() {
     let number = getRandomNum();
     if (number === 1) {
@@ -25,6 +25,9 @@ function getComputerChoice() {
 
 //HUMAN CHOICE
 
+//create prompt for user to enter one of the values
+//return the choice or error to the user
+
 const userInput = prompt("choose: rock, paper, or scissors");
 
 function getHumanChoice() {
@@ -35,5 +38,10 @@ function getHumanChoice() {
     } else if (userInput === "scissors") {
         return "you chose SCISSORS";
     } else (userInput !== null)
-        return "ERROR - check spelling.";
+        return "ERROR - check spelling / missing input";
 };
+
+//SCORE VARIABLES
+
+let humanScore = 0;
+let computerScore = 0;
