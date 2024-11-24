21e820820aca33db2854a10c162af0369bfa1c27
joey-largent <177733313+joey-largent@users.noreply.github.com>
2024-11-24 14:53:49 +0100

    add function for random number generation

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log("hello, world!")
+//computer choice
+
+//first we generate a random number between 1-3
+function getRandomNum(min, max) {
+    min = Math.ceil(1);
+    max = Math.floor(3);
+    return Math.floor(Math.random() * (3 - 1 + 1)) + 1;
+}
+
+//then we need to make a conditional statement to link the numbers with strings for R, P, and S
